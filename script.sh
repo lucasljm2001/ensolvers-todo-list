109633b1f6cf7563eceb4f31fdf55cba1a3f537f
lucasljm2001 <lucasluquitas477@gmail.com>
2021-12-28 13:58:14 -0300

    Procfile for Heroku

--- script.sh
+++ script.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
-apt update
-apt upgrade -y
-apt install nodejs
+sudo apt update
+sudo apt upgrade -y
+sudo apt install nodejs
+sudo apt install npm
+apt-get install git-all
+git clone https://github.com/lucasljm2001/ensolvers-todo-list.git
+sudo npm install npm@latest -g
+sudo npm install -g @angular/cli
+sudo apt install apache2
+sudo apt install php libapache2-mod-php php-mbstring php-xmlrpc php-soap php-gd php-xml php-cli php-zip php-bcmath php-tokenizer php-json php-pear
+sudo apt install mysql-server
+curl -sS https://getcomposer.org/installer | php
+mv composer.phar /usr/local/bin/composer
+chmod +x /usr/local/bin/composer
+npm run start 
+&
+cd ../backend-folder
+php artisan serve
